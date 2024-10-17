cb191107bee65648fd101b61f0d95154e837df67
higoshi <h33je5sg@gmail.com>
2024-10-17 19:54:12 +0900

    use prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,30 @@
 import rules from './lib/index.mjs';
 
-export default [...rules];
+export default [
+  ...rules,
+  {
+    rules: {
+      'object-curly-newline': [
+        'error',
+        {
+          ObjectExpression: {
+            multiline: true,
+            minProperties: 1,
+          },
+          ObjectPattern: {
+            multiline: true,
+            minProperties: 1,
+          },
+          ImportDeclaration: {
+            multiline: true,
+            minProperties: 1,
+          },
+          ExportDeclaration: {
+            multiline: true,
+            minProperties: 1,
+          },
+        },
+      ],
+    },
+  },
+];
