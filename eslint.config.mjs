9081f5bbfe823f50626e700ac16dbbbb2b9f38ec
higoshi <h33je5sg@gmail.com>
2024-10-18 14:12:15 +0900

    fixed rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,3 @@
 import rules from './lib/index.mjs';
 
-export default [
-  ...rules,
-  {
-    rules: {
-      'object-curly-newline': [
-        'error',
-        {
-          ObjectExpression: {
-            multiline: true,
-            minProperties: 1,
-          },
-          ObjectPattern: {
-            multiline: true,
-            minProperties: 1,
-          },
-          ImportDeclaration: {
-            multiline: true,
-            minProperties: 1,
-          },
-          ExportDeclaration: {
-            multiline: true,
-            minProperties: 1,
-          },
-        },
-      ],
-    },
-  },
-];
+export default [...rules];
